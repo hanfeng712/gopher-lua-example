67d48d2638f1359e45c1ce0adaed639dbfb3af1f
hf712 <han_feng163@163.com>
2019-01-20 08:42:12 +0000

    luatool/test1.goo提供全局函数给lua使用

--- luatool/test1.go
+++ luatool/test1.go
@@ -1,20 +1,14 @@
 package luatool
 
-import "github.com/yuin/gopher-lua"
-func Loader(L *lua.LState) int{
-	mod := L.SetFuncs(L.NewTable(), exports)
-	// register other stuff
-	L.SetField(mod, "name", lua.LString("value"))
-	L.Push(mod)
+import (
+	"github.com/yuin/gopher-lua"
+)
+
+func Add(L *lua.LState) int {
+	lv1 := L.ToInt(1)
+	//lv2 := L.ToInt(2)
+	//ret := lv1 + lv2
+	L.Push(lua.LNumber(lv1 * 2))
 	return 1
 }
 
-var exports = map[string]lua.LGFunction{
-	"myfunc": myfunc,
-}
-
-func myfunc(L *lua.LState) int {
-	//ret := lua.LString("hello word")
-	L.Push(lua.LString("3"))
-	return 1
-}
