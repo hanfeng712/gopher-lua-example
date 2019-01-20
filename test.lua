5832b5d9bc970837914a6fd93297fcbdd3212cad
hf712 <han_feng163@163.com>
2019-01-20 08:44:38 +0000

    main

--- test.lua
+++ test.lua
@@ -1,3 +1,21 @@
-local m = require("test")
-ret = m.getName()
+local ret = nil
+--go提供全局函数给lua使用
+print("=====1:go提供全局函数给lua使用=====")
+print("  ====1.1:函数没有参数")
+ret = add()
+print("  ret : " .. tostring(ret))
+print("  ====1.2:函数有参数")
+ret = double(2)
+print("  ret : " .. tostring(ret))
+
+--引用go对象和调用对象方法
+print("=====2:引用go对象和调用对象方法=====")
+local m = require("test")--引用go封装的对象
+ret = m.getName()--调用go对象方法
+print("ret : " .. tostring(ret))
+
+--go实现lua的元表功能
+print("=====3:go实现lua的元表功能=====")
+local p = person.new("hanfeng")
+ret = p:name()
 print("ret : " .. tostring(ret))
