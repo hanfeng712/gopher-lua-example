bd4bef67e5aaa41c5b81347991a925a630bdb1f0
hf712 <han_feng163@163.com>
2019-01-20 09:29:52 +0000

    go调用lua的全局方法

--- test.go
+++ test.go
@@ -21,5 +21,21 @@ func main(){
 	if err := L.DoFile("test.lua"); err != nil {
 		panic(err)
 	}
+	//go调用lua的方法
+	fmt.Print("\n\n==============================\n")
+	fmt.Print("====1:go调用lua的全局方法====\n")
+	if err := L.DoFile("test1.lua"); err != nil {
+		panic(err)
+	}
+	if err := L.CallByParam(lua.P{
+		Fn: L.GetGlobal("double"),
+		NRet: 1,
+		Protect: true,
+	}, lua.LNumber(10)); err != nil {
+		panic(err)
+	}
+	ret1 := L.Get(-1)
+	L.Pop(1)
+	fmt.Printf("ret1 : %d\n", ret1)
 	return
 }
