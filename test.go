8f8037ebfa1763d71e280f2c976646854a46b816
hf712 <han_feng163@163.com>
2019-01-20 08:44:22 +0000

    main

--- test.go
+++ test.go
@@ -11,9 +11,29 @@ func main(){
 	if err := L.DoString(`print("hello")`); err != nil {
 		    panic(err)
 	}
+	//提供全局函数给lua
+	L.SetGlobal("add", L.NewFunction(Add))
+	L.SetGlobal("double", L.NewFunction(Double))
+	//加载go提供对象给lua
 	L.PreloadModule("test", luatool.NewTestModule().Loader)
+	//加载go提供元表给lua
+	luatool.RegisterPersonType(L)
 	if err := L.DoFile("test.lua"); err != nil {
 		panic(err)
 	}
 	return
 }
+
+func Add(L *lua.LState) int {
+	lv1 := 1
+	lv2 := 2
+	ret := lv1 + lv2 
+	L.Push(lua.LNumber(ret))
+	return 1
+}
+func Double(L *lua.LState) int {
+	lv := L.ToInt(1)             /* get argument */
+	L.Push(lua.LNumber(lv * 2)) /* push result */
+	return 1                     /* number of results */
+}
+
